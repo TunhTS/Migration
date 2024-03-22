abd700163c5ef7e256e8bb3edf353f4ec31d448e
tungts <tungts@nal.vn>
2024-03-22 13:52:20 +0700

    fix

--- 2024_03_21_184038_create_inquiry_main_table.php
+++ 2024_03_21_184038_create_inquiry_main_table.php
@@ -14,7 +14,7 @@ class CreateInquiryMainTable extends Migration
     public function up()
     {
         Schema::create('inquiry_main', function (Blueprint $table) {
-            $table->bigIncrements('inquiry_id')->comment('AUTO_INCREMENT');
+            $table->unsignedInteger('inquiry_id')->autoIncrement();
             $table->unsignedSmallInteger('subsite_id')->nullable();
             $table->string('inquiry_title', 400);
             $table->string('inquiry_name', 400);
@@ -24,7 +24,8 @@ class CreateInquiryMainTable extends Migration
             $table->tinyInteger('is_completed')->default(0)->comment('0:未対応・対応中、1:対応済み');
             $table->tinyInteger('is_replied')->default(0)->comment('0:未返信、1:返信済み');
             $table->unsignedSmallInteger('updater_id')->nullable();
-            $table->timestamps();
+            $table->dateTime('created_at');
+            $table->dateTime('updated_at');
             $table->unsignedBigInteger('last_update_operation_log_id')->nullable();
         });
     }
--- 2024_03_21_190510_create_inquiry_main_attachment_file_table.php
+++ 2024_03_21_190510_create_inquiry_main_attachment_file_table.php
@@ -14,12 +14,12 @@ class CreateInquiryMainAttachmentFileTable extends Migration
     public function up()
     {
         Schema::create('inquiry_main_attachment_file', function (Blueprint $table) {
-            $table->bigIncrements('inquiry_main_attachment_file_id')->comment('AUTO_INCREMENT');
+            $table->unsignedInteger('inquiry_main_attachment_file_id')->autoIncrement();
             $table->unsignedBigInteger('inquiry_id');
             $table->string('attachment_file_name', 255);
             $table->string('attachment_file_path', 255)->comment('S3へのパス');
-            $table->unsignedBigInteger('creater_id')->nullable()->comment('投稿者から返信の場合はNull');
-            $table->dateTime('created_at')->useCurrent();
+            $table->unsignedSmallInteger('creater_id')->nullable()->comment('投稿者から返信の場合はNull');
+            $table->dateTime('created_at');
             $table->unsignedBigInteger('last_update_operation_log_id')->nullable();
         });
     }
